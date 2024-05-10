46f072452ddb075267a582a4233de48475ccdf7e
Santy2304 <a20210751@pucp.edu.pe>
2024-05-09 19:14:57 -0500

    inicio de lab

---
+++ src/main/java/com/example/gticslab5_20210751/GticsLab520210751Application.java
@@ -0,0 +1,13 @@
+package com.example.gticslab5_20210751;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab520210751Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab520210751Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gticslab5_20210751/GticsLab520210751ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticslab5_20210751;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab520210751ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
